b22dfe33c7bf3d927df3f45cb42f81b1941729de
Mrudul Deshmukh <mruduljd@gmail.com>
2025-12-12 11:44:49 +0530

    1.02

--- Assignment2_SQL_queries.sql
+++ Assignment2_SQL_queries.sql
@@ -1,18 +1,19 @@
 select * from courses 
 where date > Current_Date();
 
-Select s.reg_no,s.name,s.email,s.mobile_no,s.course_id,c.course_name
+Select s.reg_no,s.name,s.email,s.mobile_no,
+s.course_id,c.course_name
 From students s join courses c on s.course_id=c.course_id;
 
 Select s.reg_no,s.name,s.email,s.mobile_no,
-    s.profile_pic,c.course_id,c.course_name,
-    c.description AS course_description,
-    c.fees, c.start_date,
-    c.end_date, c.video_expire_days
-	from students s
-	join courses c 
-    on s.course_id = c.course_id
-	where s.email = 'student_email@gmail.com';
+s.profile_pic,c.course_id,c.course_name,
+c.description AS course_description,
+c.fees, c.start_date,
+c.end_date, c.video_expire_days
+from students s
+join courses c 
+on s.course_id = c.course_id
+where s.email = 'student_email@gmail.com';
     
 Select  c.course_id,c.course_name,c.start_date,c.end_date,c.video_expire_days,v.video_id,v.title,v.added_at
 from Courses c Join videos v on c.course_id=v.course_id
