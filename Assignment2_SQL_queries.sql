d4bed7852e5898b28c6c3b3d2cff38670e988379
Mrudul Deshmukh <mruduljd@gmail.com>
2025-12-12 11:50:31 +0530

    1.03

--- Assignment2_SQL_queries.sql
+++ Assignment2_SQL_queries.sql
@@ -1,5 +1,5 @@
 select * from courses 
-where date > Current_Date();
+where start_date > Current_Date();
 
 Select s.reg_no,s.name,s.email,s.mobile_no,
 s.course_id,c.course_name
@@ -18,7 +18,7 @@ where s.email = 'student_email@gmail.com';
 Select  c.course_id,c.course_name,c.start_date,c.end_date,c.video_expire_days,v.video_id,v.title,v.added_at
 from Courses c Join videos v on c.course_id=v.course_id
 Join students s on s.course_id=c.course_id
-where s.email = 'student_email@gmail.com' and v.added_at >= DATE_SUB(current_date, INTERVAL c.video_expire_days DAY); ;
+where s.email = 'student_email@gmail.com' and v.added_at >= DATE_SUB(current_date, INTERVAL c.video_expire_days DAY);
 
 
 
